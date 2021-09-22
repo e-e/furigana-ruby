664f0e0c0f91c54471653a7cb98743e30275c07b
Eric Ellingson <god@eric.wtf>
2021-09-21 22:40:30 -0700

    initial setup

---
+++ lib/furigana_ruby.rb
@@ -0,0 +1,137 @@
+# frozen_string_literal: true
+
+require_relative "furigana_ruby/version"
+
+module FuriganaRuby
+  class Error < StandardError; end
+
+
+  attr_reader :reading, :expression, :hiragana, :reading_html
+  ##
+  # @param [String] text
+  def initialize(text)
+    @segments = FuriganaParser.new(text).segments
+  end
+
+  def reading
+    @_reading ||= @segments.map(&:reading).join("")
+  end
+
+  def expression
+    @_expression ||= @segments.map(&:expression).join("")
+  end
+
+  def hiragana
+    @_hiragana ||= @segments.map(&:hiragana).join("")
+  end
+
+  def reading_html
+    @_reading_html ||= @segments.map(&:reading_html).join("")
+  end
+
+  class FuriganaParser
+    attr_reader :segments
+
+    ##
+    # @param [String] reading
+    def initialize(reading)
+      @segments = []
+      @current_base = ""
+      @current_furigana = ""
+      @parsing_base_section = true
+      @characters = reading.present? && reading.length ? reading.split("") : []
+
+      process
+    end
+
+    def process
+      while @characters.length > 0
+        current = @characters.shift
+
+        if current == "["
+          @parsing_base_section = false
+        elsif current == "]"
+          next_segment
+        elsif last_character_in_block?(current, @characters) && @parsing_base_section
+          @current_base += current
+          next_segment
+        elsif !@parsing_base_section
+          @current_furigana += current
+        else
+          @current_base += current
+        end
+      end
+
+      next_segment
+    end
+
+    def next_segment
+      if @current_base.length > 0
+        @segments << get_segment(@current_base, @current_furigana)
+      end
+
+      @current_base = ""
+      @current_furigana = ""
+      @parsing_base_section = true
+    end
+
+    ##
+    # @param [String] base_text
+    # @param [String] furigana
+    def get_segment(base_text, furigana)
+      # puts("get_segment/furigana: #{furigana}")
+      unless furigana.present?
+        return UndecoratedSegment.new(base_text)
+      end
+
+      FuriganaSegment.new(base_text, furigana)
+    end
+
+    ##
+    # @param [String] current
+    # @param [Array<String>] character_list
+    # @return [Boolean]
+    def last_character_in_block?(current, character_list)
+      character_list.length == 0 ||
+        (
+          kanji?(current) != kanji?(character_list[0]) &&
+            character_list[0] != "["
+        )
+    end
+
+    ##
+    # @return [Boolean]
+    def kanji?(character)
+      char = character[0, 1]
+      character.present? && char.ord >= 0x4e00 && char.ord <= 0x9faf
+    end
+  end
+
+  class FuriganaSegment
+    attr_reader :expression, :hiragana, :reading, :reading_html
+
+    ##
+    # @param [String] base_text
+    # @param [String] furigana
+    def initialize(base_text, furigana)
+      @expression = base_text
+      @hiragana = furigana.strip
+      @reading = base_text + "[" + furigana + "]"
+      @reading_html = "<ruby><rb>" + base_text + "</rb><rt>" + furigana + "</rt></ruby>"
+    end
+  end
+
+  class UndecoratedSegment
+    attr_reader :expression, :hiragana, :reading, :reading_html
+
+    ##
+    # @param [String] base_text
+    def initialize(base_text)
+      # puts("Undecorated: #{base_text}")
+      @expression = base_text
+      @hiragana = base_text
+      @reading = base_text
+      @reading_html = base_text
+    end
+  end
+end
---
+++ spec/furigana_ruby_spec.rb
@@ -0,0 +1,226 @@
+# frozen_string_literal: true
+require "spec_helper"
+
+RSpec.describe FuriganaRuby do
+  it "single gem that spans the entire word" do
+    reading = "動物[どうぶつ]"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq(reading)
+  end
+
+  it "single gem in middle of a word" do
+    reading = "新[あたら]しい"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq(reading)
+  end
+
+  it "multiple gems inside word" do
+    reading = "黒[くろ]熊[くま]"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq(reading)
+  end
+
+  it "readings without gems do not change" do
+    reading = "ライオン"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq(reading)
+  end
+
+  it "honorific should not be included in gem" do
+    reading = "お茶[ちゃ]"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq(reading)
+    expect(furigana.hiragana).to eq("おちゃ")
+    expect(furigana.expression).to eq("お茶")
+    expect(furigana.reading_html).to eq("お<ruby><rb>茶</rb><rt>ちゃ</rt></ruby>")
+  end
+
+  it "honorific should not be included in gem2" do
+    reading = "起[お]きます"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq(reading)
+    expect(furigana.hiragana).to eq("おきます")
+    expect(furigana.expression).to eq("起きます")
+    expect(furigana.reading_html).to eq("<ruby><rb>起</rb><rt>お</rt></ruby>きます")
+  end
+
+  it "number should not be included in gem" do
+    reading = "9時[じ]"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq(reading)
+    expect(furigana.hiragana).to eq("9じ")
+    expect(furigana.expression).to eq("9時")
+    expect(furigana.reading_html).to eq("9<ruby><rb>時</rb><rt>じ</rt></ruby>")
+  end
+
+  it "punctuation should not be included in gem" do
+    reading = "大[おお]きい。犬[いぬ]"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq(reading)
+    expect(furigana.hiragana).to eq("おおきい。いぬ")
+    expect(furigana.expression).to eq("大きい。犬")
+    expect(furigana.reading_html).to eq("<ruby><rb>大</rb><rt>おお</rt></ruby>きい。<ruby><rb>犬</rb><rt>いぬ</rt></ruby>")
+  end
+
+  it "romaji should not be included in gem" do
+    reading = "Big犬[いぬ]"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq(reading)
+    expect(furigana.hiragana).to eq("Bigいぬ")
+    expect(furigana.expression).to eq("Big犬")
+    expect(furigana.reading_html).to eq("Big<ruby><rb>犬</rb><rt>いぬ</rt></ruby>")
+  end
+
+  it "katana should not be included in gem" do
+    reading = "ローマ字[じ]"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq(reading)
+    expect(furigana.hiragana).to eq("ローマじ")
+    expect(furigana.expression).to eq("ローマ字")
+    expect(furigana.reading_html).to eq("ローマ<ruby><rb>字</rb><rt>じ</rt></ruby>")
+  end
+
+  it "hiragana should not be included in gem" do
+    reading = "売[う]り場[ば]"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq(reading)
+    expect(furigana.hiragana).to eq("うりば")
+    expect(furigana.expression).to eq("売り場")
+    expect(furigana.reading_html).to eq("<ruby><rb>売</rb><rt>う</rt></ruby>り<ruby><rb>場</rb><rt>ば</rt></ruby>")
+  end
+
+  it "お in furigana is not treated as honorific" do
+    reading = "起[お]きます"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq(reading)
+    expect(furigana.hiragana).to eq("おきます")
+    expect(furigana.expression).to eq("起きます")
+    expect(furigana.reading_html).to eq("<ruby><rb>起</rb><rt>お</rt></ruby>きます")
+  end
+
+  it "honorific in middle of a phrase" do
+    reading = "東京[とうきょう] お急行[きゅうこう]"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq(reading)
+  end
+
+  it "honorific in middle of a word" do
+    reading = "東京[とうきょう]お急行[きゅうこう]"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq(reading)
+  end
+
+  it "honorific at end of a word" do
+    reading = "茶[ちゃ]お"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq(reading)
+  end
+
+  it "space can be a delimiter between hiragana sections" do
+    reading = "あの 人[ひと]"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq(reading)
+  end
+
+  it "preserve a space between segments" do
+    reading = "東京[とうきょう] 急行[きゅうこう]"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq(reading)
+    expect(furigana.expression).to eq("東京 急行")
+    expect(furigana.hiragana).to eq("とうきょう きゅうこう")
+    expect(furigana.reading_html).to eq("<ruby><rb>東京</rb><rt>とうきょう</rt></ruby> <ruby><rb>急行</rb><rt>きゅうこう</rt></ruby>")
+  end
+
+  it "preserve multiple spaces between segments" do
+    reading = "東京[とうきょう]    急行[きゅうこう]"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq("東京[とうきょう]    急行[きゅうこう]")
+  end
+
+  it "last character in reading is space" do
+    reading = "東京[とうきょう] 急行[きゅうこう]"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq(reading)
+  end
+
+  it "ignore empty furigana section" do
+    reading = "あの[]人[ひと]"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq("あの人[ひと]")
+  end
+
+  it "ignore furigana with only whitespace" do
+    reading = "あの[ ]人[ひと]"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq("あの人[ひと]")
+  end
+
+  it "furigana to expression" do
+    reading = "動物[どうぶつ]"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.expression).to eq("動物")
+  end
+
+  it "furigana to hiragana" do
+    reading = "新[あたら]しい"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.hiragana).to eq("あたらしい")
+  end
+
+  it "furigana to html ruby" do
+    reading = "新[あたら]しい"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading_html).to eq("<ruby><rb>新</rb><rt>あたら</rt></ruby>しい")
+  end
+
+  it "empty furigana" do
+    reading = ""
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading).to eq(reading)
+    expect(furigana.expression).to eq("")
+    expect(furigana.hiragana).to eq("")
+    expect(furigana.reading_html).to eq("")
+  end
+
+  it "null furigana" do
+    reading = ""
+    furigana = FuriganaRuby.new(nil)
+
+    expect(furigana.reading).to eq(reading)
+    expect(furigana.expression).to eq("")
+    expect(furigana.hiragana).to eq("")
+    expect(furigana.reading_html).to eq("")
+  end
+
+  it "allow html inside string" do
+    reading = "学生です<span class='particle'>か</span>。"
+    furigana = FuriganaRuby.new(reading)
+
+    expect(furigana.reading_html).to eq(reading)
+  end
+end
